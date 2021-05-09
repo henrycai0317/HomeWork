1ee3790eeddc8633ed013678c74efd821e5c4552
henrycai <alaba.havai@gmail.com>
2021-05-09 12:20:54 +0800

    Finished Second Home work

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter number of tickets: ");
-        int tics=scanner.nextInt();
-        System.out.print("How many round-trip tickets: ");
-        int roundtrip=scanner.nextInt();
-
-        Tickets tickets = new Tickets(tics,roundtrip);
-        tickets.show();
+            Scanner scanner = new Scanner(System.in);
+            int tics=0;
+            while (tics !=-1){
+            System.out.print("Please enter number of tickets: ");
+           tics=scanner.nextInt();
+           if(tics == -1){
+               System.out.println("Thank you for coming ,see you next time ～");
+           }else {
+            System.out.print("How many round-trip tickets: ");
+            int roundtrip=scanner.nextInt();
+            Tickets tickets = new Tickets(tics,roundtrip);
+            tickets.show();
+           }
+        }
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,23 +3,23 @@ package com.train
 import java.util.*
 
 fun main() {
-
-    val  scn = Scanner(System.`in`)
+    var tickets =0;
+    while(tickets != -1){
     print("Please enter number of tickets:")
-    val tickets= scn.nextInt()
+        tickets= readLine()!!.toInt()
+        if(tickets ==-1){
+            println("Thank you for coming ,see you next time ～")
+        }else{
     print("How many round-trip tickets: ");
-    val roundTrip=scn.nextInt()
-
-     var tic = TrainTic(tickets,roundTrip)
+        var roundTrip= readLine()!!.toInt()
+       var tic = TrainTic(tickets,roundTrip)
      tic.show()
-
+        }
+    }
 }
 class TrainTic(var tickets: Int,var roundTrip:Int){
-
     fun show(){
-        val total =(tickets-roundTrip)*1000+(roundTrip*2000)*0.9
-        println("Total tickets :"+tickets+"\n"
-                +"Round-trip:"+roundTrip+"\n"
-                +"Total:"+total)
+        println("Total tickets :$tickets\nRound-trip :$roundTrip\nTotal:${getTotal()}")
     }
+    private fun getTotal()= (tickets - roundTrip) * 1000 + (roundTrip * 2000) * 0.9
 }
